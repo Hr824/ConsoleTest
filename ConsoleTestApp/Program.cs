02bcfb2d20988929b49b6376c1c52c439e4316c0
Hr824 <bdv.pro@gmail.com>
2020-01-21 19:50:09 +0100

    New method Add() to add 2 integers

--- ConsoleTestApp/Program.cs
+++ ConsoleTestApp/Program.cs
@@ -12,5 +12,11 @@ namespace ConsoleTestApp
         {
             List<int> numbers = new List<int> { 10, 20, 30, 40, 50 };
         }
+
+
+        private int Add(int number1, int number2)
+        {
+            return number1 + number2;
+        }
     }
 }
